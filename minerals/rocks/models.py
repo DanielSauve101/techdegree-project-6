e11b561a773d46f2c1db65bce947bf71349b7170
Daniel Sauve <sauve.daniel.157@gmail.com>
2019-05-31 15:35:28 -0400

    added load file for initial data load from json.

--- minerals/rocks/models.py
+++ minerals/rocks/models.py
@@ -1,3 +1,28 @@
-from django.db import models
+from django.db import models, migrations
 
 # Create your models here.
+
+
+class Minerals(models.Model):
+    name = models.CharField(max_length=100)
+    image_filename = models.TextField()
+    image_caption = models.TextField(blank=True, default='')
+    category = models.TextField(blank=True, default='')
+    formula = models.TextField(blank=True, default='')
+    strunz_classification = models.TextField(blank=True, default='')
+    color = models.TextField(blank=True, default='')
+    crystal_system = models.TextField(blank=True, default='')
+    unit_cell = models.TextField(blank=True, default='')
+    crystal_symmetry = models.TextField(blank=True, default='')
+    cleavage = models.TextField(blank=True, default='')
+    mohs_scale_hardness = models.TextField(blank=True, default='')
+    luster = models.TextField(blank=True, default='')
+    streak = models.TextField(blank=True, default='')
+    diaphaneity = models.TextField(blank=True, default='')
+    optical_properties = models.TextField(blank=True, default='')
+    refractive_index = models.TextField(blank=True, default='')
+    crystal_habit = models.TextField(blank=True, default='')
+    specific_gravity = models.TextField(blank=True, default='')
+
+    def __str__(self):
+        return self.name
