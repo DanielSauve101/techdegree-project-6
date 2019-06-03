d85573935e18d153ac5fa840d3d8ae90b8e96e8d
Daniel Sauve <sauve.daniel.157@gmail.com>
2019-06-03 12:34:38 -0400

    added Paginator to mineral_list view and adjusted mineral_list.html to include the paginator.

--- minerals/minerals/views.py
+++ minerals/minerals/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render
 
 from rocks.models import Minerals
@@ -6,5 +7,9 @@ from rocks.models import Minerals
 
 
 def mineral_list(request):
-    minerals = Minerals.objects.all()
+    """View to show the mineral list using Paginator to limit 45 per page"""
+    mineral_list = Minerals.objects.all()
+    paginator = Paginator(mineral_list, 45)
+    page = request.GET.get('page')
+    minerals = paginator.get_page(page)
     return render(request, 'mineral_list.html', {'minerals': minerals})
