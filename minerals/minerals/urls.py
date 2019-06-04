0f17e71f17377bcb4a1bb0ab2cb333a2483113f4
Daniel Sauve <sauve.daniel.157@gmail.com>
2019-06-04 15:03:19 -0400

    Changed static to work with STATIC_URL

--- minerals/minerals/urls.py
+++ minerals/minerals/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path, re_path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from rocks import views
 
@@ -22,4 +24,4 @@ urlpatterns = [
     path('', views.mineral_list, name='home'),
     re_path(r'^minerals/(?P<pk>\d+)/$', views.mineral_detail, name='detail'),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
