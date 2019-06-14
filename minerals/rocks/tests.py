5f83224e75f1ffab0e8212809300ffc3aa2c34f0
Daniel Sauve <sauve.daniel.157@gmail.com>
2019-06-14 11:58:33 -0400

    revised rocks/tests.py to test new mineral_detail view.

--- minerals/rocks/tests.py
+++ minerals/rocks/tests.py
@@ -26,6 +26,7 @@ class MineralViewsTests(TestCase):
             refractive_index='x\y03c9 = 1.597',
             crystal_habit='Forms crust-like aggregates',
             specific_gravity='34.20 - 18.22',
+            group='sulphites'
         )
 
     def test_mineral_list_view(self):
@@ -38,10 +39,10 @@ class MineralViewsTests(TestCase):
     def test_mineral_detail_view(self):
         resp = self.client.get(reverse('detail',
                                        kwargs={'pk': self.mineral.pk}))
-        important_dict = {'name': 'A', 'image_filename': 'mineralx.jpg',
-                          'image_caption': 'this is the mineral x'}
-        other_dict = {'category': 'xyz',
-                      'formula': 'X<sub>42</sub>Y<sub>32</sub>',
+        main_dict = {'name': 'A', 'image_filename': 'mineralx.jpg',
+                     'image_caption': 'this is the mineral x'}
+        important_dict = {'category': 'xyz', 'group': 'sulphites'}
+        other_dict = {'formula': 'X<sub>42</sub>Y<sub>32</sub>',
                       'strunz_classification': '15.XY.57',
                       'color': 'red',
                       'crystal_system': 'crystal',
@@ -57,6 +58,7 @@ class MineralViewsTests(TestCase):
                       'crystal_habit': 'Forms crust-like aggregates',
                       'specific_gravity': '34.20 - 18.22'}
         self.assertEqual(resp.status_code, 200)
+        self.assertEqual(main_dict, resp.context['main'])
         self.assertEqual(important_dict, resp.context['important'])
         self.assertEqual(other_dict, resp.context['other'])
         self.assertTemplateUsed(resp, 'rocks/mineral_detail.html')
